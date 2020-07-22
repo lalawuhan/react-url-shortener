fdc3459de332bd7b34ea13778fbbf38523b7cb9e
Rethabile Segoe <precious.segoe@gmail.com>
2020-07-22 13:45:53 +0200

    changed data to a function

--- data/data.js
+++ data/data.js
@@ -1,19 +1,18 @@
 const fs = require("fs");
-const path = require("path");
 
-let data = [];
-try {
-  const linksFilePath = path.join(__dirname, "./links.json");
-  data = JSON.parse(fs.readFileSync(linksFilePath, "utf8"));
-} catch (err) {
-  if (err.code === "ENOENT") {
-    const err = new Error("File does not exist.");
-    err.status = 404;
-    throw err;
-  } else {
-    const err = new Error("Error reading data");
-    throw err;
+module.exports = function getData() {
+  let data = [];
+  try {
+    data = JSON.parse(fs.readFileSync("data/links.json", "utf8"));
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      const err = new Error("File does not exist.");
+      err.status = 404;
+      throw err;
+    } else {
+      const err = new Error("Error reading data");
+      throw err;
+    }
   }
-}
-
-module.exports.data = data;
+  return data;
+};
